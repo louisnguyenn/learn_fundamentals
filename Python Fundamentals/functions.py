fd479f88300b1af6532634612c24e4454faa0ad5
louis nguyen <louis.nguyen550@gmail.com>
2025-06-29 18:40:24 -0400

    simple functions

---
+++ Python Fundamentals/functions.py
@@ -0,0 +1,9 @@
+# defining a function
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome")
+
+
+# calling the function
+greet("Louis", "Nguyen")
+greet("John", "Smith")
