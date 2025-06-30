ecdc273101f7e2289f0b4ab77bf7e30713295069
louis nguyen <louis.nguyen550@gmail.com>
2025-06-29 23:42:49 -0400

    changing text content with get element by id

--- Javascript Fundamentals/webpage/index.js
+++ Javascript Fundamentals/webpage/index.js
@@ -1,6 +1,8 @@
-console.log("Hello")
-console.log("I like pizza")
+// console.log("Hello")
+// console.log("I like pizza")
 
-window.alert(`This is a alert!`);
-window.alert(`I like pizza!`);
+// window.alert(`This is a alert!`);
+// window.alert(`I like pizza!`);
 
+document.getElementById("header").textContent = `Hello`;
+document.getElementById("paragraph").textContent = `I like pizza`;
