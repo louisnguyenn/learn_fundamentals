0dda08d570e975901f89039bbf487081d551e8d4
louis nguyen <louis.nguyen550@gmail.com>
2025-06-29 23:50:29 -0400

    variables

--- Javascript Fundamentals/webpage/index.js
+++ Javascript Fundamentals/webpage/index.js
@@ -4,5 +4,25 @@
 // window.alert(`This is a alert!`);
 // window.alert(`I like pizza!`);
 
-document.getElementById("header").textContent = `Hello`;
-document.getElementById("paragraph").textContent = `I like pizza`;
+// document.getElementById("header").textContent = `Hello`;
+// document.getElementById("paragraph").textContent = `I like pizza`;
+
+// let x = 123;
+// let age = 19;
+// let price = 10.99;
+
+// console.log(typeof price)
+// console.log(x)
+// console.log(age)
+// console.log(price)
+// console.log(`Your age is ${age} and the price is $${price}`);
+
+// let firstName = "Louis Nguyen";
+// let food = "pizza";
+
+// console.log(typeof firstName);
+// console.log(firstName);
+// console.log(`Your first name: ${firstName}`);
+// console.log(food);
+// console.log(`You, ${firstName}, like ${food}`);
+
