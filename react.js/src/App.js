dc74e8d9aa23551e68b4975e4fa2611c61cb0106
louis nguyen <louis.nguyen550@gmail.com>
2025-07-26 14:58:54 -0400

    usestate

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastname}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1>Name: {props.name}</h1>
+//       <h2>Last Name: {props.lastname}</h2>
+//       <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// }
 
 const App = () => {
   // const name = "Jane";
   // const isNameShowing = true;
   // const isUserLoggedIn = true;
+  const [counter, setCounter] = useState(0);  // provide initial value in the function
 
   return (
     <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
       {/* <h1>Hello, {isNameShowing ? name : "Someone"}</h1>
       {name ? (
         <>
@@ -28,8 +33,8 @@ const App = () => {
           <h2>There is no name</h2>
         </>
       )} */}
-      <Person name={"John"} lastname={"Doe"} age={"20"}/>
-      <Person name={"Louis"} lastname={"Nguyen"} age={"19"}/>
+      {/* <Person name={"John"} lastname={"Doe"} age={"20"}/>
+      <Person name={"Louis"} lastname={"Nguyen"} age={"19"}/> */}
     </div>
   );
 }
