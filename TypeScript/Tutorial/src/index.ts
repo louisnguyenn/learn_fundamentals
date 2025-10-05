904743db4f624084b185eb0c618db44e7815cca0
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-05 09:12:28 -0400

    union types

--- TypeScript/Tutorial/src/index.ts
+++ TypeScript/Tutorial/src/index.ts
@@ -33,19 +33,31 @@
 
 // calculateTax(10_000);
 
-type Employee = {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
-};
+// type Employee = {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// };
 
-let employee: Employee = {
-  id: 1,
-  name: 'Louis',
-  retire: (date: Date) => {
-    console.log(date);
-  },
-};
+// let employee: Employee = {
+//   id: 1,
+//   name: 'Louis',
+//   retire: (date: Date) => {
+//     console.log(date);
+//   },
+// };
 
 // employee.id = 0;
 // employee.name = 'Louis';
+
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === 'number') {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
