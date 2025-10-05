b59b00e202cfd75d7eff2d16e71f743b5c47013e
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-04 21:33:11 -0400

    enums

---
+++ TypeScript/Tutorial/src/index.ts
@@ -0,0 +1,25 @@
+// let age: number = 13;
+
+// if (age <= 13)
+// {
+//   console.log('Hello World');
+// }
+
+// let numbers: number[] = [1, 2, 3];
+// numbers.forEach(n => n)
+
+// let user: [number, string, boolean, number] = [1, 'Louis', true, 0];
+// user.push(1);
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
