83aa867f92da4633504c0dc7e6030f36faea9059
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-04 21:28:02 -0400

    tuples

--- Typescript/Tutorial/src/index.ts
+++ Typescript/Tutorial/src/index.ts
@@ -1,2 +1,12 @@
-let numbers: number[] = [1, 2, 3];
-numbers.forEach(n => n.)
+// let age: number = 13;
+
+// if (age <= 13)
+// {
+//   console.log('Hello World');
+// }
+
+// let numbers: number[] = [1, 2, 3];
+// numbers.forEach(n => n)
+
+let user: [number, string, boolean, number] = [1, 'Louis', true, 0];
+user.push(1);
